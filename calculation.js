569a12c52530bf5e0cdb9ec2a21130192db3a6a7
Sunil Rana <muskaan.rana@gmail.com>
2018-06-14 19:05:18 +0530

    node commit

---
+++ calculation.js
@@ -0,0 +1,2 @@
+exports.sum = (a, b) => (a + b);
+exports.subtract = (a, b) => (a - b);
---
+++ index.js
@@ -0,0 +1,8 @@
+//var calc = {
+//    sum: (a, b) => (a + b),
+//    subtract:(a,b) => (a-b)
+
+//}
+var calc = require("./calculation.js")
+console.log("sum is " + calc.sum(2,5));
+console.log("subtract is " + calc.subtract(5,1));
